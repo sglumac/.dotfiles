e1dec8e0465efa2cec900bf582674b2931d46f5f
Slaven Glumac <sglumac@dspace.hr>
2026-02-06 15:59:53 +0100

    buffer keymaps

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -18,6 +18,9 @@ return {
 
     vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>', { silent = true })
     vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>bo', '<Cmd>BufferCloseAllButCurrent<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>bp', '<Cmd>BufferCloseAllButPinned<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>bn', '<Cmd>BufferPin<CR>', { silent = true })
   end,
 
 }
