5f769e715c3eaf103699f939523ed675d168af5d
Slaven Glumac <sglumac@dspace.hr>
2026-02-16 14:55:40 +0100

    Changing debug prefix

--- nvim/.config/nvim/lua/plugins/mini-clue.lua
+++ nvim/.config/nvim/lua/plugins/mini-clue.lua
@@ -6,44 +6,44 @@ return {
 
     miniclue.setup({
       triggers = {
-        -- { mode = 'n', keys = '<C-w>' },
         { mode = 'n', keys = '<C-w>' },
-        { mode = 'n', keys = '<leader>d,' },
+        { mode = 'n', keys = '<leader>d' },
         { mode = 'n', keys = '<leader>' },
       },
 
       clues = {
+        -- Window Hydra
         {
           mode = 'n',
-          -- keys = { '<C-w>', group = 'Window Hydra' },
           keys = { '<C-w>', group = 'Window Hydra' },
           desc = 'Window commands',
         },
-        { mode = 'n', keys = '<C-w>h', postkeys = '<C-w>', action = '<C-w>h',  desc = 'move left' },
-        { mode = 'n', keys = '<C-w>j', postkeys = '<C-w>', action = '<C-w>j',  desc = 'move down' },
-        { mode = 'n', keys = '<C-w>k', postkeys = '<C-w>', action = '<C-w>k',  desc = 'move up' },
-        { mode = 'n', keys = '<C-w>l', postkeys = '<C-w>', action = '<C-w>l',  desc = 'move right' },
-        { mode = 'n', keys = '<C-w>H', postkeys = '<C-w>', action = '<C-w><',  desc = 'decrease width' },
-        { mode = 'n', keys = '<C-w>J', postkeys = '<C-w>', action = '<C-w>-',  desc = 'decrease height' },
-        { mode = 'n', keys = '<C-w>K', postkeys = '<C-w>', action = '<C-w>+',  desc = 'increase height' },
-        { mode = 'n', keys = '<C-w>L', postkeys = '<C-w>', action = '<C-w>>',  desc = 'increase width' },
-        { mode = 'n', keys = '<C-w>=', postkeys = '<C-w>', action = '<C-w>=',  desc = 'equalize windows' },
-        { mode = 'n', keys = '<C-w>v', postkeys = '<C-w>', action = '<C-w>=',  desc = 'split vertical' },
-        { mode = 'n', keys = '<C-w>s', postkeys = '<C-w>', action = '<C-w>=',  desc = 'split horizontal' },
-        { mode = 'n', keys = '<C-w>q', action = '<Esc>',       desc = 'exit window/hydra' },
+        { mode = 'n', keys = '<C-w>h', postkeys = '<C-w>', action = '<C-w>h',         desc = 'move left' },
+        { mode = 'n', keys = '<C-w>j', postkeys = '<C-w>', action = '<C-w>j',         desc = 'move down' },
+        { mode = 'n', keys = '<C-w>k', postkeys = '<C-w>', action = '<C-w>k',         desc = 'move up' },
+        { mode = 'n', keys = '<C-w>l', postkeys = '<C-w>', action = '<C-w>l',         desc = 'move right' },
+        { mode = 'n', keys = '<C-w>H', postkeys = '<C-w>', action = '<C-w><',         desc = 'decrease width' },
+        { mode = 'n', keys = '<C-w>J', postkeys = '<C-w>', action = '<C-w>-',         desc = 'decrease height' },
+        { mode = 'n', keys = '<C-w>K', postkeys = '<C-w>', action = '<C-w>+',         desc = 'increase height' },
+        { mode = 'n', keys = '<C-w>L', postkeys = '<C-w>', action = '<C-w>>',         desc = 'increase width' },
+        { mode = 'n', keys = '<C-w>=', postkeys = '<C-w>', action = '<C-w>=',         desc = 'equalize windows' },
+        { mode = 'n', keys = '<C-w>v', postkeys = '<C-w>', action = '<C-w>=',         desc = 'split vertical' },
+        { mode = 'n', keys = '<C-w>s', postkeys = '<C-w>', action = '<C-w>=',         desc = 'split horizontal' },
+        { mode = 'n', keys = '<C-w>q', action = '<Esc>',   desc = 'exit window/hydra' },
+        -- Debug Hydra
         {
           mode = 'n',
-          keys = { '<leader>d,', group = 'Debug Hydra' },
+          keys = { '<leader>d', group = 'Debug Hydra' },
           desc = 'Debug commands',
         },
-        { mode = 'n', keys = '<leader>d,o', postkeys = '<leader>d,', action = function() require('dap').step_out() end,  desc = 'step out' },
-        { mode = 'n', keys = '<leader>d,n', postkeys = '<leader>d,', action = function() require('dap').step_over() end, desc = 'step over' },
-        { mode = 'n', keys = '<leader>d,k', postkeys = '<leader>d,', action = function() require('dap').step_back() end, desc = 'step back' },
-        { mode = 'n', keys = '<leader>d,s', postkeys = '<leader>d,', action = function() require('dap').step_into() end, desc = 'step into' },
+        { mode = 'n', keys = '<leader>do', postkeys = '<leader>d', action = function() require('dap').step_out() end,  desc = 'step out' },
+        { mode = 'n', keys = '<leader>dn', postkeys = '<leader>d', action = function() require('dap').step_over() end, desc = 'step over' },
+        { mode = 'n', keys = '<leader>dk', postkeys = '<leader>d', action = function() require('dap').step_back() end, desc = 'step back' },
+        { mode = 'n', keys = '<leader>ds', postkeys = '<leader>d', action = function() require('dap').step_into() end, desc = 'step into' },
         {
           mode = 'n',
-          keys = '<leader>d,b',
-          postkeys = '<leader>d,',
+          keys = '<leader>db',
+          postkeys = '<leader>d',
           action = function()
             require('dap')
                 .toggle_breakpoint()
@@ -52,18 +52,18 @@ return {
         },
         {
           mode = 'n',
-          keys = '<leader>d,t',
-          postkeys = '<leader>d,',
+          keys = '<leader>dt',
+          postkeys = '<leader>d',
           action = function()
             require('dap')
                 .clear_breakpoints()
           end,
           desc = 'clear breakpoints'
         },
-        { mode = 'n', keys = '<leader>d,c', postkeys = '<leader>d,', action = function() require('dap').continue() end,  desc = 'continue' },
-        { mode = 'n', keys = '<leader>d,x', postkeys = '<leader>d,', action = function() require('dap').terminate() end, desc = 'terminate' },
-        { mode = 'n', keys = '<leader>d,r', postkeys = '<leader>d,', action = function() require('dap').repl.open() end, desc = 'open repl' },
-        { mode = 'n', keys = '<leader>d,q', action = '<Esc>',        desc = 'exit hydra' },
+        { mode = 'n', keys = '<leader>dc', postkeys = '<leader>d', action = function() require('dap').continue() end,  desc = 'continue' },
+        { mode = 'n', keys = '<leader>dx', postkeys = '<leader>d', action = function() require('dap').terminate() end, desc = 'terminate' },
+        { mode = 'n', keys = '<leader>dr', postkeys = '<leader>d', action = function() require('dap').repl.open() end, desc = 'open repl' },
+        { mode = 'n', keys = '<leader>dq', action = '<Esc>',        desc = 'exit hydra' },
       },
     })
   end,
