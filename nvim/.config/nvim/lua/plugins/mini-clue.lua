2676698ee5ef674e27c77135f22aa12f87a3d0f1
Slaven Glumac <sglumac@dspace.hr>
2026-02-23 13:50:14 +0100

    Debug last config

--- nvim/.config/nvim/lua/plugins/mini-clue.lua
+++ nvim/.config/nvim/lua/plugins/mini-clue.lua
@@ -4,6 +4,17 @@ return {
   config = function()
     -- Debug mappings
     local dap = require('dap')
+    local function run_last_config()
+      local ft = vim.bo.filetype
+      local configs = dap.configurations[ft]
+
+      if not configs or #configs == 0 then
+        return vim.notify("No DAP configurations for filetype: " .. ft, vim.log.levels.WARN)
+      end
+
+      local last = configs[#configs]
+      dap.run(last)
+    end
     vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Step Out' })
     vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = 'Step Over' })
@@ -15,6 +26,8 @@ return {
     vim.keymap.set('n', '<leader>du', dap.up, { desc = 'Stack Up' })
     vim.keymap.set('n', '<leader>dd', dap.down, { desc = 'Stack Down' })
     vim.keymap.set("n", "<leader>df", "<cmd>Telescope dap configurations<CR>", { desc = "Debug Configurations" })
+    vim.keymap.set('n', '<leader>dr', run_last_config, { desc = 'Run Last Config' })
+
 
     local miniclue = require('mini.clue')
 
@@ -50,22 +63,23 @@ return {
         miniclue.gen_clues.z(),
         -- Windows
         miniclue.gen_clues.windows(),
-        { mode = 'n', keys = '<C-w>w',     postkeys = '<C-w>',                                action = '<C-w>w',            desc = 'move next' },
-        { mode = 'n', keys = '<C-w><',     postkeys = '<C-w>',                                action = '<C-w><',            desc = 'decrease width' },
-        { mode = 'n', keys = '<C-w>-',     postkeys = '<C-w>',                                action = '<C-w>-',            desc = 'decrease height' },
-        { mode = 'n', keys = '<C-w>+',     postkeys = '<C-w>',                                action = '<C-w>+',            desc = 'increase height' },
-        { mode = 'n', keys = '<C-w>>',     postkeys = '<C-w>',                                action = '<C-w>>',            desc = 'increase width' },
+        { mode = 'n', keys = '<C-w>w',     postkeys = '<C-w>',                               action = '<C-w>w',            desc = 'move next' },
+        { mode = 'n', keys = '<C-w><',     postkeys = '<C-w>',                               action = '<C-w><',            desc = 'decrease width' },
+        { mode = 'n', keys = '<C-w>-',     postkeys = '<C-w>',                               action = '<C-w>-',            desc = 'decrease height' },
+        { mode = 'n', keys = '<C-w>+',     postkeys = '<C-w>',                               action = '<C-w>+',            desc = 'increase height' },
+        { mode = 'n', keys = '<C-w>>',     postkeys = '<C-w>',                               action = '<C-w>>',            desc = 'increase width' },
         -- Debug
-        { mode = 'n', keys = '<leader>do', postkeys = '<leader>d',                            action = dap.step_out,        desc = 'step out' },
-        { mode = 'n', keys = '<leader>dn', postkeys = '<leader>d',                            action = dap.step_over,       desc = 'step over' },
-        { mode = 'n', keys = '<leader>ds', postkeys = '<leader>d',                            action = dap.step_into,       desc = 'step into' },
-        { mode = 'n', keys = '<leader>dc', postkeys = '<leader>d',                            action = dap.continue,        desc = 'continue' },
-        { mode = 'n', keys = '<leader>du', postkeys = '<leader>d',                            action = dap.up,              desc = 'stack up' },
-        { mode = 'n', keys = '<leader>dd', postkeys = '<leader>d',                            action = dap.down,            desc = 'stack down' },
-        { mode = 'n', keys = '<leader>db', action = dap.toggle_breakpoint,                    desc = 'toggle breakpoint' },
-        { mode = 'n', keys = '<leader>dt', action = dap.clear_breakpoints,                    desc = 'clear breakpoints' },
-        { mode = 'n', keys = '<leader>dq', action = dap.terminate,                            desc = 'terminate' },
-        { mode = 'n', keys = '<leader>df', action = "<cmd>Telescope dap configurations<CR>",  desc = 'debug configurations' },
+        { mode = 'n', keys = '<leader>do', postkeys = '<leader>d',                           action = dap.step_out,        desc = 'step out' },
+        { mode = 'n', keys = '<leader>dn', postkeys = '<leader>d',                           action = dap.step_over,       desc = 'step over' },
+        { mode = 'n', keys = '<leader>ds', postkeys = '<leader>d',                           action = dap.step_into,       desc = 'step into' },
+        { mode = 'n', keys = '<leader>dc', postkeys = '<leader>d',                           action = dap.continue,        desc = 'continue' },
+        { mode = 'n', keys = '<leader>du', postkeys = '<leader>d',                           action = dap.up,              desc = 'stack up' },
+        { mode = 'n', keys = '<leader>dd', postkeys = '<leader>d',                           action = dap.down,            desc = 'stack down' },
+        { mode = 'n', keys = '<leader>db', action = dap.toggle_breakpoint,                   desc = 'toggle breakpoint' },
+        { mode = 'n', keys = '<leader>dt', action = dap.clear_breakpoints,                   desc = 'clear breakpoints' },
+        { mode = 'n', keys = '<leader>dq', action = dap.terminate,                           desc = 'terminate' },
+        { mode = 'n', keys = '<leader>df', action = "<cmd>Telescope dap configurations<CR>", desc = 'debug configurations' },
+        { mode = 'n', keys = '<leader>dr', action = run_last_config,                         desc = 'run last config' },
       },
     })
   end,
